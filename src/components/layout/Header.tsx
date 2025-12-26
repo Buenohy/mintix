386822a9e0daed35a46c613c9a66c3febcb18a40
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-12-26 20:56:13 -0300

    feat: Header with logo and Web3 link

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,6 +7,8 @@ import SearchBar from "./SearchBar";
 import User from "./User";
 
 import { useUser } from "@/hooks/use-events";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
   const { data: userData, isLoading } = useUser();
@@ -15,10 +17,21 @@ export default function Header() {
   return (
     <nav className="w-full bg-[#0f1521] px-3.5 py-6 md:px-7.5">
       <div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
-        <h2 className="text-sm font-normal text-gray-400 md:text-base">
-          Event Management /
-          <span className="font-bold text-white"> Events</span>
-        </h2>
+        <div className="flex items-center gap-4">
+          <Link href="/">
+            <Image
+              src="/images/mintix-logo.png"
+              alt="Mintix Logo"
+              width={140}
+              height={50}
+            />
+          </Link>
+          <div>
+            <Link href="/web3">
+              <h2 className="text-primary-mintix text-2xl">Web3</h2>
+            </Link>
+          </div>
+        </div>
 
         <div className="flex items-center gap-4 lg:gap-8">
           <div className="w-full md:w-64 lg:w-80">
@@ -35,7 +48,9 @@ export default function Header() {
                 "/images/smiling-woman-looking-front-delighted.png"
               }
             />
-            <WalletMultiButton />
+            <div className="hidden sm:flex">
+              <WalletMultiButton />
+            </div>
           </div>
         </div>
       </div>
